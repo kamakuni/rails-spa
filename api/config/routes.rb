9ccb64402926c4498ee838f68b31f5a188da1897
sugawara kensuke <superradio86@gmail.com>
2022-06-12 23:33:53 +0900

    add sessions controller

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,25 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'sessions/new'
       get 'cards/index'
       get 'cards/create'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
       get 'lists/index'
       get 'lists/create'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
       get 'users/index'
       get 'users/create'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
