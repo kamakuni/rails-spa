11c544a5e7aee24750e63bd11925c46d4c3dcbeb
sugawara kensuke <superradio86@gmail.com>
2022-06-23 23:18:25 +0900

    update docker-compose.yml for the frontend app.

---
+++ front/app/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import SignUp from './components/SignUp/SingnUp';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div >
+  );
+}
+
+export default App;
