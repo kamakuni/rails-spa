ef882c0c00261aec29421202f56ed472dff6a3db
sugawara kensuke <superradio86@gmail.com>
2022-06-26 21:38:45 +0900

    remove unnecessary params.

--- api/app/controllers/api/v1/users_controller.rb
+++ api/app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 # UsersController
 class Api::V1::UsersController < ApplicationController
   def create
-    user = User.create!(name: params[:name], email: params[:email], password: params[:password])
+    user = User.create!(email: params[:email], password: params[:password])
     render json: { name: user.name, email: user.email }
   rescue ActiveRecord::RecordInvalid => e
     render status: 400, json: { message: e.message }
