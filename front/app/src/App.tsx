b21d5f3352bdd270583fbf129292f299e14f3a86
sugawara kensuke <superradio86@gmail.com>
2022-06-27 23:55:38 +0900

    add NavLink.

--- front/app/src/App.tsx
+++ front/app/src/App.tsx
@@ -6,21 +6,30 @@ import Dashboard from './components/Dashboard/Dashboard';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
+import { NavLink } from 'react-router-dom';
 
 function App() {
+
   return (
     <div className="wrapper">
       <h1>App</h1>
       <BrowserRouter>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/dashboard">Dashboard</NavLink>
+          <NavLink to="/login">LogIn</NavLink>
+          <NavLink to="/signup">SignUp</NavLink>
+        </nav>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/register" element={<SignUp />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </div >
   );
+
 }
 
 export default App;
