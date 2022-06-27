92394540eec34226c6edcb35ed5617622eb18345
sugawara kensuke <superradio86@gmail.com>
2022-06-27 23:56:32 +0900

    implement Dashboard component.

--- front/app/src/components/Dashboard/Dashboard.tsx
+++ front/app/src/components/Dashboard/Dashboard.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import axios from 'axios';
+import { createReadStream } from 'fs';
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function Dashboard() {
-    return (
-        <h2>Dashboard</h2>
-    );
+
+    const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+    const navigate = useNavigate();
+    // TODO
+    axios.get('http://localhost:3000/api/v1/login', {
+        withCredentials: true
+    }).then((res) => {
+        console.log(res)
+        setIsLoggedIn(true)
+    }).catch((e) => {
+        console.log(e)
+        setIsLoggedIn(false)
+    })
+
+    useEffect(() => {
+        if (!isLoggedIn) {
+            navigate("/login");
+        }
+    })
+
+    if (isLoggedIn) {
+        return (<h2>Dashboard</h2>);
+    } else {
+        return (<h2>Access forbidden.</h2>);
+    }
+
 }
 
 export default Dashboard;
