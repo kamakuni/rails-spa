7d3a5ce660e46255cfe21f349d836fa4a29dce45
sugawara kensuke <superradio86@gmail.com>
2022-06-29 22:48:09 +0900

    remove codes for authentication.

--- front/app/src/components/Dashboard/Dashboard.tsx
+++ front/app/src/components/Dashboard/Dashboard.tsx
@@ -4,32 +4,7 @@ import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 function Dashboard() {
-
-    const [isLoggedIn, setIsLoggedIn] = React.useState(false);
-    const navigate = useNavigate();
-    // TODO
-    axios.get('http://localhost:3000/api/v1/login', {
-        withCredentials: true
-    }).then((res) => {
-        console.log(res)
-        setIsLoggedIn(true)
-    }).catch((e) => {
-        console.log(e)
-        setIsLoggedIn(false)
-    })
-
-    useEffect(() => {
-        if (!isLoggedIn) {
-            navigate("/login");
-        }
-    })
-
-    if (isLoggedIn) {
-        return (<h2>Dashboard</h2>);
-    } else {
-        return (<h2>Access forbidden.</h2>);
-    }
-
+    return (<h2>Dashboard</h2>);
 }
 
 export default Dashboard;
