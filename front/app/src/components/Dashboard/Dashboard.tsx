c9ebab164f2fd91234b541f90c406914070f7c98
sugawara kensuke <superradio86@gmail.com>
2022-07-02 23:03:24 +0900

    Add elements for Lists to Dashboard component.

--- front/app/src/components/Dashboard/Dashboard.tsx
+++ front/app/src/components/Dashboard/Dashboard.tsx
@@ -1,10 +1,53 @@
 import axios from 'axios';
 import { createReadStream } from 'fs';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
+interface List {
+    title: string
+}
+
 function Dashboard() {
-    return (<h2>Dashboard</h2>);
+
+    const [title, setTitle] = useState("")
+    const [lists, setLists] = useState<Array<List>>([])
+
+    const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value)
+    }
+
+    const handleAddListClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        const copyed = [...lists]
+        copyed.push({ title: title })
+        setLists(copyed)
+        console.log(title)
+    }
+
+    return (
+        <div>
+            <h2>Dashboard</h2>
+            <div>
+                <div>
+                    <label>title</label>
+                    <input onChange={handleTitleChange}></input>
+                </div>
+                <button onClick={handleAddListClick}>Add Lists</button>
+            </div>
+            <h3>List</h3>
+            <div>
+                <ul>
+                    {lists.map((list, i) => {
+                        return <li>
+                            <label>{list.title}</label>
+                            <div>
+                                <button>Add Cards</button>
+                                <button>Remove List</button>
+                            </div>
+                        </li>
+                    })}
+                </ul>
+            </div>
+        </div>);
 }
 
 export default Dashboard;
