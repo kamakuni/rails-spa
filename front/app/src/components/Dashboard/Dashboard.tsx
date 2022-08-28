b33d12060e0b1c942bf39c52430f60a875d35e7d
sugawara kensuke <superradio86@gmail.com>
2022-08-28 23:24:08 +0900

    fix Dashboard element.

--- front/app/src/components/Dashboard/Dashboard.tsx
+++ front/app/src/components/Dashboard/Dashboard.tsx
@@ -29,11 +29,11 @@ function Card(props: CardProps) {
     )
 }
 
-interface ListItemPros {
+interface ListItemProps {
     list: List,
 }
 
-function ListItem(props: ListItemPros) {
+function ListItem(props: ListItemProps) {
 
     const [card, setCard] = useState({ title: "", body: "" })
 
@@ -104,8 +104,6 @@ function ListItem(props: ListItemPros) {
 function Dashboard() {
 
     const [title, setTitle] = useState("")
-    const [card, setCard] = useState({ title: "", body: "" })
-    //    const [lists, setLists] = useState<Array<List>>([])
 
     const { isLoading, isSuccess, lists } = useAppSelector((state) => state.dashboard)
     const dispatch = useAppDispatch();
@@ -117,39 +115,6 @@ function Dashboard() {
     const handleAddListClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         dispatch(createList({ title: title }))
         setTitle("")
-    }
-
-
-    const handleRemoveList = (i: number) => {
-        const filtered = lists.filter((l, index) => { return i != index })
-        dispatch(removeList(lists[i].id))
-    }
-
-    const handleSaveCards = (i: number, j: number) => {
-        const copyed = [...lists]
-        //    copyed[i].cards = copyed[i].cards.filter((c, index) => { return j != index })
-        //    setLists(copyed)
-    }
-
-    const handleRemoveCards = (i: number, j: number) => {
-        dispatch(removeCard({ list_id: lists[i].id, card_id: lists[i].cards[j].id }))
-    }
-
-    const handleAddCards = (i: number) => {
-        dispatch(createCard({ title: card.title, body: card.body, list_id: lists[i].id }))
-        setCard({ title: "", body: "" })
-    }
-
-    const handleCardTitleChange = (value: string) => {
-        const copyed = { ...card }
-        copyed.title = value
-        setCard(copyed)
-    }
-
-    const handleCardBodyChange = (value: string) => {
-        const copyed = { ...card }
-        copyed.body = value
-        setCard(copyed)
     }
 
     useEffect(() => {
@@ -180,46 +145,9 @@ function Dashboard() {
             <h3>List</h3>
             <div>
                 <ul>
-                    {lists.map((list, i) => {
-                        return <li key={i}>
+                    {lists.map((list, index) => {
+                        return <li key={index}>
                             <ListItem list={list} ></ListItem>
-                        </li>
-                    })}
-                </ul>
-            </div>
-            <div>
-                <ul>
-                    {lists.map((list, i) => {
-                        return <li key={i}>
-                            <label>{list.title}</label>
-                            <div>
-                                <label>title:</label>
-                                <input onChange={(e) => handleCardTitleChange(e.target.value)}></input>
-                            </div>
-                            <div>
-                                <label>body:</label>
-                                <input onChange={(e) => handleCardBodyChange(e.target.value)}></input>
-                            </div>
-                            <div>
-                                <button onClick={() => handleAddCards(i)} >Add Cards</button>
-                            </div>
-                            <div>
-                                <ul>
-                                    {list.cards.map((card, j) => {
-                                        return (
-                                            <li key={j}>
-                                                <Card
-                                                    body={card.body}
-                                                    title={card.title} />
-                                                <button onClick={() => handleRemoveCards(i, j)} >Delete Cards</button>
-                                            </li>
-                                        )
-                                    })}
-                                </ul>
-                            </div>
-                            <div>
-                                <button onClick={() => handleRemoveList(i)}>Remove List</button>
-                            </div>
                         </li>
                     })}
                 </ul>
