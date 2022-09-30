45f09c77e87d3b17266a6dc9a4df18d914c8f44f
sugawara kensuke <superradio86@gmail.com>
2022-10-01 00:36:42 +0900

    fix mock for cards api.

--- front/app/src/components/Dashboard/Dashboard.test.tsx
+++ front/app/src/components/Dashboard/Dashboard.test.tsx
@@ -45,6 +45,12 @@ describe("test for dashboard", () => {
       )
     }),
     rest.get("http://localhost:3000/api/v1/cards", async (req, res, ctx) => {
+      const list_id = req.url.searchParams.get("list_id")
+      if (list_id !== "1") {
+        return res(
+          ctx.status(200)
+        );
+      }
       return res(
         ctx.status(200),
         ctx.json(
@@ -67,13 +73,23 @@ describe("test for dashboard", () => {
   beforeAll(() => server.listen())
   afterEach(() => server.resetHandlers())
   afterAll(() => server.close())
-  test("renders list items", async () => {
+  test("renders list title", async () => {
     render(
       <Provider store={store}>
         <Dashboard />
       </Provider>
     );
     const l1 = await screen.findByText("l1");
-    expect(l1).toHaveLength(1)
+    expect(l1).not.toBe(null);
   })
+  test("renders card title", async () => {
+    render(
+      <Provider store={store}>
+        <Dashboard />
+      </Provider>
+    );
+    const t1 = await screen.findByText("t1");
+    expect(t1).not.toBe(null);
+  })
+
 })
