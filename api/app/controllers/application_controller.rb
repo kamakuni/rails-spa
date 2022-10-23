910f43616e5e3e354247bdfff241dd9658051a3d
sugawara kensuke <superradio86@gmail.com>
2022-10-23 23:42:53 +0900

    add method for CSRF protection

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 # ApplicationController
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  before_action :has_xhr_header!
+
   def authenticate_user!
     render status: 401, json: { message: "Users is unauthorized." } unless session[:user_id]
+  end
+
+  private
+  def has_xhr_header!
+    render status: 403, json: { message: "Forbidden."} unless request.xhr?
   end
 end
